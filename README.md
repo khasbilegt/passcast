0c7da87047e3abe9a41ba23635f2c2032bf3a867
Khasbilegt.TS <khasbilegt.ts@gmail.com>
2021-08-10 00:23:33 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# passcast
-pass \(the standard unix password manager\) integration for Raycast
+## passcast
+
+1. Install and setup pass (https://www.passwordstore.org)
+2. Install Raycast (https://www.raycast.com)
+3. Save the script file on your machine and add the folder containing the script in Raycast's settings
---
+++ passcast.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title pass
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Path (e.g Email/zx2c4.com)" }
+# @raycast.argument2 { "type": "text", "placeholder": "Password", "secure": true }
+# @raycast.packageName Utils
+
+# Documentation:
+# @raycast.description Unix pass password manager integration for Raycast
+# @raycast.author Khasbilegt.TS
+# @raycast.authorURL https://github.com/khasbilegt
+
+import subprocess
+import sys
+
+from pathlib import Path
+
+PASSWORD_STORE_PATH = Path("~/.password-store").expanduser()
+PASSWORD_PATH = (PASSWORD_STORE_PATH / Path(f"{sys.argv[1]}.gpg")).resolve()
+
+try:
+    if PASSWORD_PATH.exists():
+        subprocess.run(
+            ["gpgconf", "--reload", "gpg-agent"],
+            check=True,
+            capture_output=True,
+        )
+        content = subprocess.run(
+            [
+                "gpg",
+                "--decrypt",
+                "--pinentry-mode",
+                "loopback",
+                "--passphrase",
+                f"{sys.argv[2]}",
+                f"{PASSWORD_PATH}",
+            ],
+            text=True,
+            check=True,
+            capture_output=True,
+        )
+        copied = subprocess.run(
+            ["pass", "-c", f"{sys.argv[1]}"],
+            text=True,
+            check=True,
+            capture_output=True,
+        )
+
+        print(f"🔑 {content.stdout}")
+        print(f"🤖 {copied.stdout}")
+    else:
+        print("🔴 Password not found!")
+except subprocess.CalledProcessError as e:
+    print(f"{e.stderr}")
