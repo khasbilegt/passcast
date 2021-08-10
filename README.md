2de6ae4513fc3b12b5b6243925afb810937afa2d
enkhee-Osiris <enkhee.ag@gmail.com>
2021-08-10 11:08:26 +0800

    Update readme

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-## passcast
+# passcast
 
-1. Install and setup pass (https://www.passwordstore.org)
-2. Install Raycast (https://www.raycast.com)
-3. Save the script file on your machine and add the folder containing the script in Raycast's settings
+## Requirements
+
+- [x] gpg
+- [x] [pass](https://www.passwordstore.org) `brew install pass`
+- [x] [Raycast](https://www.raycast.com) `brew install --cask raycast`
+
+## To install
+
+```sh
+$ curl https://raw.githubusercontent.com/khasbilegt/passcast/main/install.sh -sSf | sh
+```
+
+Or you can manually download `passcast.py` and put it into your desired directory.
+
+To add your script to Raycast open `Raycast Preferrences`, goto `Extensions`, click `Add (+)` or (CMD + N), click `Add Script Directory`, choose your directory (if you've installed with script `~/.config/raycast/scripts`)
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# shellcheck shell=dash
+set -u
+
+command -v "curl" > /dev/null 2>&1 || { echo "curl command did not found" >&2; exit 1; }
+command -v "rm" > /dev/null 2>&1 || { echo "rm command did not found" >&2; exit 1; }
+command -v "mkdir" > /dev/null 2>&1 || { echo "mkdir command did not found" >&2; exit 1; }
+command -v "rmdir" > /dev/null 2>&1 || { echo "rmdir command did not found" >&2; exit 1; }
+command -v "pass" > /dev/null 2>&1 || { echo "pass command did not found" >&2; exit 1; }
+
+OUTPUT_PATH="${HOME}/.config/raycast/scripts"
+
+main() {
+    if [ ! -d "${HOME}/.password-store" ]; then
+        echo "${HOME}/.password-store directory did not found" >&2;
+        exit 1;
+    fi
+
+    if [ ! -d "${HOME}/.config" ]; then
+        mkdir -p "${HOME}/.config"
+    fi
+
+    if [ -d "${HOME}/.config/raycast" ]; then
+        if [ -d "${OUTPUT_PATH}" ]; then
+            # Removing outdated version
+            rmdir "${OUTPUT_PATH}"
+        fi
+
+        mkdir -p "${OUTPUT_PATH}"
+    else
+        mkdir -p "${OUTPUT_PATH}"
+    fi
+
+    curl https://raw.githubusercontent.com/khasbilegt/passcast/main/passcast.py -o "${OUTPUT_PATH}/passcast.py"
+}
+
+main || exit 1
